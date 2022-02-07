3cbae3f56641876f71e16667fcdefec19971d038
John Nooney <1803534@uad.ac.uk>
2022-02-07 00:05:46 +0000

    Update convert ms to hours

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,25 +9,33 @@ namespace TimeToSleep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the amount of time before going to sleep. \nie: <1.5> for 1 and half hours");
+            Console.WriteLine("Enter the amount of time before going to sleep. \nie: <1.5> for 1 and half hours");
             string input = Console.ReadLine();
-            double price;
+            double time;
 
-            if(input != null && Double.TryParse(input, out price))
+            if(input != null && Double.TryParse(input, out time))
             {
+                double hours = time * 3600000; //ms to hours
+
+                Console.WriteLine("Understood, going to sleep in " + input + " hours.");
+
                 Timer aTimer = new Timer();
                 aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-                aTimer.Interval = input;
+                aTimer.Interval = hours; 
                 aTimer.Enabled = true;
 
-                SetSuspendState(false, true, true);
+            }
+            else
+            {
+                Console.WriteLine("I don't understand. Please try again.");
             }
         }
 
         // Specify what you want to happen when the Elapsed event is raised.
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Going to sleep now... good night.");
+            SetSuspendState(false, true, true);
         }
 
         [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
